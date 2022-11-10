da717a7ee9f1200f54065b2825ad927317722a98
erpslayer <111244114+MossesT@users.noreply.github.com>
2022-11-11 01:06:18 +0300

    debug

--- droga_finance/models/credit_limit.py
+++ droga_finance/models/credit_limit.py
@@ -4,7 +4,7 @@ from odoo.exceptions import ValidationError
 
 class cust_credit_limit(models.Model):
     _inherit='res.partner'
-    cust_credit_limit = fields.Float('Credit limit')
+    cust_credit_limit = fields.Float(string='Credit limit')
     unsettled_amount = fields.Float(string='Unsettled amount')
     available_amount = fields.Float(string='Available credit')
 
