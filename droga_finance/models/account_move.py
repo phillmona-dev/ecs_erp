6f4fab84c94476248828263e6e8214255ab21232
Binyam Assefa <binyam@afomsoft.com>
2022-12-07 13:41:31 +0300

    witholding report updated

--- droga_finance/models/account_move.py
+++ droga_finance/models/account_move.py
@@ -7,8 +7,47 @@ class AccountMove(models.Model):
 
     _inherit = "account.move"
 
+    @api.depends("amount_total")
+    def _compute_amount_word(self):
+        for record in self:
+            record.untaxed_amount_word = str(
+                record.currency_id.amount_to_text(record.amount_untaxed))
+            record.amount_total_word = str(
+                record.currency_id.amount_to_text(record.amount_total))
+            if record.withholding_two_percent != 0:
+                record.tax_amount_word = str(
+                    record.currency_id.amount_to_text(record.withholding_two_percent))
+            elif record.withholding_thirty_percent != 0:
+                record.tax_amount_word = str(
+                    record.currency_id.amount_to_text(record.withholding_thirty_percent))
+
+    @api.depends("amount_total")
+    def _comput_witholding_amount(self):
+        tax_amount1 = 0
+        tax_amount2 = 0
+        for record in self.line_ids:
+            if record.name == 'Purchase withholding 2%':
+                tax_amount1 += abs(record.balance)
+            elif record.name == 'Purchase withholding 30%':
+                tax_amount2 += abs(record.balance)
+
+        self.withholding_two_percent = tax_amount1
+        self.withholding_thirty_percent = tax_amount2
+
     transaction_type = fields.Many2one("account.transaction.type")
     transaction_no = fields.Char("Transaction Number", default='New')
+    untaxed_amount_word = fields.Char(
+        compute='_compute_amount_word', store=True)
+    amount_total_word = fields.Char(
+        compute='_compute_amount_word', store=True)
+    tax_amount_word = fields.Char(
+        compute='_compute_amount_word', store=True)
+
+    withholding_two_percent = fields.Float(
+        compute='_comput_witholding_amount', store=True)
+    withholding_thirty_percent = fields.Float(
+        compute='_comput_witholding_amount', store=True)
+    
 
     @api.model
     def create(self, vals):
--- droga_finance/report/witholding_report.py
+++ droga_finance/report/witholding_report.py
@@ -14,6 +14,8 @@ class WitholdingReport(models.TransientModel):
 
     _name = 'droga.finance.witholding.excel.report'
 
+    report = fields.Selection(
+        [('Payable', 'Payable'), ('Receivable', 'Receivable')])
     date_from = fields.Date("Date From", required=True)
     date_to = fields.Date("Date To")
     company_id = fields.Many2one('res.company', 'Company', required=True,
@@ -26,7 +28,10 @@ class WitholdingReport(models.TransientModel):
         # This generates our excel file
         file_io = BytesIO()
         workbook = xlsxwriter.Workbook(file_io)
-        self.generate_xlsx_report(workbook)
+        if self.report == 'Payable':
+            self.generate_payable_xlsx_report(workbook)
+        else:
+            self.generate_receivable_xlsx_report(workbook)
         workbook.close()
 
         # The file to download will be stored under fileout field
@@ -45,8 +50,8 @@ class WitholdingReport(models.TransientModel):
                 self.id) + '&field=fileout&download=true&filename=' + filename,
         }
 
-    def generate_xlsx_report(self, workbook):
-        sheet = workbook.add_worksheet('Witholding Report')
+    def generate_payable_xlsx_report(self, workbook):
+        sheet = workbook.add_worksheet('Witholding Report Payable')
 
         sheet.set_column('A:A', 25)
         sheet.set_column('B:B', 15)
@@ -134,11 +139,109 @@ class WitholdingReport(models.TransientModel):
             witholdee_tin = record.partner_id.vat if record.partner_id.vat else ''
             reciept_no = record.ref if record.ref else ''
 
-            sheet.write(row_start, 0, "9063340002")
-            sheet.write(row_start, 1, witholdee_tin)
-            sheet.write(row_start, 2, witholdee_name)
+            sheet.write(row_start, 0, "9063340002", border)
+            sheet.write(row_start, 1, witholdee_tin, border)
+            sheet.write(row_start, 2, witholdee_name, border)
             sheet.write(row_start, 3, reciept_no, border)
             sheet.write(row_start, 4, record.date, date_format)
             sheet.write(row_start, 5, record.tax_base_amount, num_format)
-            sheet.write(row_start, 6, record.balance, num_format)
+            sheet.write(row_start, 6, abs(record.balance), num_format)
+            row_start += 1
+
+    def generate_receivable_xlsx_report(self, workbook):
+        sheet = workbook.add_worksheet('Witholding Report Receivable')
+
+        sheet.set_column('A:A', 25)
+        sheet.set_column('B:B', 15)
+        sheet.set_column('C:C', 30)
+        sheet.set_column('D:D', 15)
+        sheet.set_column('E:E', 15)
+        sheet.set_column('F:F', 20)
+        sheet.set_column('G:G', 20)
+        row_start = 0
+        date_format = workbook.add_format(
+            {'num_format': 'dd/mm/yyyy', 'border': 7})
+        num_format = workbook.add_format({'num_format': 43, 'border': 7})
+        cent_format = workbook.add_format({'num_format': 41, 'border': 7})
+        border = workbook.add_format({'border': 7})
+        bold = workbook.add_format({'bold': True})
+        header_format = workbook.add_format({
+            'bold': 1,
+            'border': 0,
+            'align': 'center',
+            'valign': 'vcenter',
+            'font_size': 22})
+        main_title_format = workbook.add_format({
+            'bold': 1,
+            'border': 0,
+            'align': 'center',
+            'valign': 'vcenter',
+            'font_size': 16})
+        parameter_format = workbook.add_format({
+            'bold': 1,
+            'border': 7,
+            'align': 'left',
+            'valign': 'vcenter',
+            'font_size': 12,
+            'fg_color': '#F6F5F5'})
+
+        separator_format = workbook.add_format({
+            'bold': 1,
+            'border': 7,
+            'align': 'left',
+            'valign': 'vcenter',
+            'font_size': 12,
+            'fg_color': '#D9D9D9'})
+
+        title_format = workbook.add_format({
+            'bold': 1,
+            'border': 1,
+            'align': 'center',
+            'valign': 'vcenter',
+            'font_size': 11,
+            'text_wrap': 1,
+            'fg_color': '#F6F5F5'})
+        title_format_num = workbook.add_format({
+            'bold': 1,
+            'border': 1,
+            'num_format': 43,
+            'align': 'center',
+            'valign': 'vcenter',
+            'font_size': 11,
+            'text_wrap': 1,
+            'fg_color': '#F6F5F5'})
+
+        # search RFQ
+
+        account_moves = self.env['account.move'].search(
+            [('date', '>=', self.date_from), ('date', '<=', self.date_to), ('company_id', '=', self.company_id.id)])
+
+        accounts = self.env['account.account'].search(
+            [('code', 'in', ('116001', '116001'))])
+
+        withholdings = account_moves.line_ids.search(
+            [('account_id', 'in', accounts.ids)])
+
+        sheet.write(row_start, 0, 'Withholder''s Tax Account', title_format)
+        sheet.write(row_start, 1, 'Withholdee''s TIN', title_format)
+        sheet.write(row_start, 2, 'Withholdee''s Name', title_format)
+        sheet.write(row_start, 3, 'Receipt No', title_format)
+        sheet.write(row_start, 4, 'Receipt Date', title_format)
+        sheet.write(row_start, 5, 'Taxable Amount', title_format)
+        sheet.write(row_start, 6, 'Tax Withheld', title_format)
+        row_start += 1
+
+        for record in withholdings:
+            # get witholde name and tin no
+            witholdee_name = record.partner_id.name if record.partner_id.vat else ''
+            witholdee_tin = record.partner_id.vat if record.partner_id.vat else ''
+            reciept_no = record.ref if record.ref else ''
+
+            sheet.write(row_start, 0, "9063340002", border)
+            sheet.write(row_start, 1, witholdee_tin, border)
+            sheet.write(row_start, 2, witholdee_name, border)
+            sheet.write(row_start, 3, reciept_no, border)
+            sheet.write(row_start, 4, record.date, date_format)
+            sheet.write(row_start, 5, record.tax_base_amount, num_format)
+            sheet.write(row_start, 6, abs(record.balance), num_format)
             row_start += 1
