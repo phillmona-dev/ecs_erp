6a1bc1c75b1d90a37a6193b3c7226912005735d0
erpslayer <111244114+MossesT@users.noreply.github.com>
2022-12-16 03:15:21 +0300

    CRM

--- droga_crm/models/customer_contacts.py
+++ droga_crm/models/customer_contacts.py
@@ -27,8 +27,8 @@ class droga_crm_contacts(models.Model):
 
     def _get_descr(self):
         for record in self:
-            name = record.job_position.job_position+ ' - ' if record.job_position else ''
-            name=name+record.specialty.specialty+ ' - ' if record.specialty.specialty else name
+            name = (record.job_position.job_position+ ' - ') if record.job_position else ''
+            name=(name+record.specialty.specialty+ ' - ') if record.specialty.specialty else name
 
             record.descr= name+record.contact_name
             
