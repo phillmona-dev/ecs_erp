a7bfed5203446d8d64964d3a97ee520a158d7025
Binyam Assefa <binyam@afomsoft.com>
2022-12-25 11:48:41 +0300

    CConfiguration updated for finance,budget and self service

--- droga_self_service/__manifest__.py
+++ droga_self_service/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','droga_procurement','droga_inventory'],
+    'depends': ['base','account_budget','droga_procurement','droga_inventory'],
 
     # always loaded
     'data': [
