a26635dfa83e379c727a1b46ae8d015b08cc7dde
erpslayer <111244114+MossesT@users.noreply.github.com>
2022-12-27 06:42:51 +0300

    Sales
    Sales

--- droga_sales/models/discount_rules.py
+++ droga_sales/models/discount_rules.py
@@ -9,7 +9,7 @@ class droga_price_discount_per_type(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin', 'image.mixin']
     cust_type = fields.Many2one('droga.cust.type',string='Customer type',tracking=True)
     product_group = fields.Many2one('product.category',string='Product category',tracking=True)
-    percent = fields.Float(string='Percentage (+ve or -ve)',tracking=True)
+    percent = fields.Float(string='Percentage (+ve or -ve)',tracking=True,digits=(12, 9))
     core_products_or_all= fields.Selection([('Core', 'Core products'), ('Noncore', 'Non-core products'),('All', 'All')],string='Core?',required=True,default='Core',tracking=True)
     status = fields.Selection([('Active', 'Active'), ('Closed', 'Closed')],required=True,default='Active',tracking=True)
 
