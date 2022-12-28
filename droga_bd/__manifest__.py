bd041bd63120914665a4481f7cfdf4aed2b1536c
Binyam Assefa <binyam@afomsoft.com>
2022-12-28 09:50:58 +0300

    Payment request form updated

--- droga_bd/__manifest__.py
+++ droga_bd/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','account_budget' ,'droga_procurement','droga_inventory'],
+    'depends': ['base','account_budget' ,'droga_procurement','droga_inventory','droga_finance'],
 
     # always loaded
     'data': [
