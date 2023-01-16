0e152118e8ac79ecef66bc3404aa2825b8f02fe9
erpslayer <111244114+MossesT@users.noreply.github.com>
2023-01-16 10:26:09 +0300

    Sales payment bug
    Sales payment bug

--- droga_sales/models/deliv_hide_before_pay.py
+++ droga_sales/models/deliv_hide_before_pay.py
@@ -3,19 +3,4 @@ from odoo import models,fields,api
 class droga_sales_invoice_payment(models.Model):
     _inherit = 'account.payment'
 
-    @api.model
-    def create(self,vals):
-        res=super(droga_sales_invoice_payment, self).create(vals)
-        try:
-            invoice=self.env['account.move'].search([('name','=',vals['ref'])])
-        except:
-            return res
-        if len(invoice)>0:
-            sp=self.env['stock.picking'].sudo().search([('origin','=',invoice[0]['invoice_origin'])])
-            if len(sp)>0:
-                for rec in sp:
-                    if not rec.delivery_order_show:
-                        rec.write({'delivery_order_show': True})
-                        rec.action_assign()
-        return res
-
+    
