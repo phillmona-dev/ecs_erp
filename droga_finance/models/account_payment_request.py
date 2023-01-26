e779cc9de021667222cf687910f49f042b2a860a
Binyam Assefa <binyam@afomsoft.com>
2023-01-26 23:46:13 +0300

    Convert to word updated on sales & finance module

--- droga_finance/models/account_payment_request.py
+++ droga_finance/models/account_payment_request.py
@@ -1,7 +1,6 @@
 from odoo import _, api, fields, models
 from datetime import datetime
 from odoo.exceptions import ValidationError
-from odoo.custom_addons.droga.droga_utility.utility import convert_to_word
 
 
 class PaymentRequest(models.Model):
@@ -127,7 +126,7 @@ class PaymentRequest(models.Model):
     # @api.depends('total_amount', 'exchange_rate')
     def _compute_amount_to_word(self):
         for record in self:
-            record.amount_in_word = str(convert_to_word(
+            record.amount_in_word = str(self.convert_to_word(
                 record.total_amount * record.exchange_rate))
 
     def submit_request(self):
@@ -307,3 +306,80 @@ class PaymentRequest(models.Model):
             'type': 'ir.actions.act_window',
             'target': 'current',
         }
+
+    def convert_to_word(self, number):
+        number = str(number)
+        int_side = number
+        dec_side = ''
+        for i in range(0, len(number)):
+            if number[i] == '.':
+                int_side = number[:i]
+                dec_side = number[i + 1:]
+                break
+        while not (int_side.isdigit()) or not (dec_side.isdigit()) and dec_side != '':
+            dec_side = ''
+            # print('Only numbers are allowed! (decimals included, but not fractions)')
+            int_side = number
+            for i in range(0, len(number)):
+                if number[i] == '.':
+                    int_side = number[:i]
+                    dec_side = number[i + 1:]
+            user_choice = input()
+        int_length = len(int_side)
+        ones = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ']
+        teens = ['ten ', 'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ',
+                 'eighteen ',
+                 'nineteen ']
+        decades = ['', '', 'twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety ']
+        hundreds = ['', 'one hundred ', 'two hundred ', 'three hundred ', 'four hundred ', 'five hundred ',
+                    'six hundred ',
+                    'seven hundred ', 'eight hundred ', 'nine hundred ']
+        comma = ['thousand ', 'million ', 'trillion ', 'quadrillion ']
+        word = ''
+        int_length = len(int_side)
+        dec_length = len(dec_side)
+        change = int_length
+        up_change = 0
+        while change > 0:
+            if int_side == '':
+                break
+            if number == '0':
+                word = 'zero'
+                break
+            elif change > 1 and int_side[change - 2] == '1':
+                for i in range(0, 10):
+                    if int_side[change - 1] == str(i):
+                        word = teens[i] + word
+            else:
+                if change > 0:
+                    for i in range(0, 10):
+                        if int_side[change - 1] == str(i):
+                            word = ones[i] + word
+                if change > 1:
+                    for i in range(0, 10):
+                        if int_side[change - 2] == str(i):
+                            word = decades[i] + word
+            if change > 2:
+                for i in range(0, 10):
+                    if int_side[change - 3] == str(i):
+                        word = hundreds[i] + word
+            if change > 3:
+                word = comma[up_change] + word
+            change -= 3
+            up_change += 1
+        # word += 'birr '
+
+        print(dec_side)
+        """
+        for i in range(0, len(dec_side)):
+            for x in range(0, 10):
+                if dec_side[i] == str(x):
+                    word += ones[x]"""
+
+        if dec_side not in ['', '0', '00']:
+            word += 'birr and '
+            word += self.convert_to_word(dec_side) + " cents"
+
+        word += " only"
+
+        return word.title()
--- droga_sales/models/account_move.py
+++ droga_sales/models/account_move.py
@@ -5,7 +5,6 @@ import xml.etree.cElementTree as ET
 from datetime import datetime
 import requests
 from odoo import api, fields, models
-from odoo.custom_addons.droga.droga_utility.utility import convert_to_word
 
 
 class account_move(models.Model):
@@ -31,14 +30,14 @@ class account_move(models.Model):
     def _get_total_amount_word(self):
         for record in self:
             # convert amount to word
-            record.total_amount_word = convert_to_word(record.amount_total)
+            record.total_amount_word = self.convert_to_word(record.amount_total)
 
     @api.depends("partner_id")
     def get_tin_no(self):
         for record in self:
             record.tin_no = record.partner_id.vat
             # convert amount to word
-            record.amount_total_word = convert_to_word(record.amount_total)
+            record.amount_total_word = self.convert_to_word(record.amount_total)
 
     @api.depends('invoice_payment_term_id')
     def _get_so_type(self):
@@ -194,6 +193,83 @@ class account_move(models.Model):
     def print_sales_attachment(self):
         res1 = self.env.ref('droga_sales.droga_sales_pos_attachment_action').report_action(self)
         return res1
+
+    def convert_to_word(self, number):
+        number = str(number)
+        int_side = number
+        dec_side = ''
+        for i in range(0, len(number)):
+            if number[i] == '.':
+                int_side = number[:i]
+                dec_side = number[i + 1:]
+                break
+        while not (int_side.isdigit()) or not (dec_side.isdigit()) and dec_side != '':
+            dec_side = ''
+            # print('Only numbers are allowed! (decimals included, but not fractions)')
+            int_side = number
+            for i in range(0, len(number)):
+                if number[i] == '.':
+                    int_side = number[:i]
+                    dec_side = number[i + 1:]
+            user_choice = input()
+        int_length = len(int_side)
+        ones = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ']
+        teens = ['ten ', 'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ',
+                 'eighteen ',
+                 'nineteen ']
+        decades = ['', '', 'twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety ']
+        hundreds = ['', 'one hundred ', 'two hundred ', 'three hundred ', 'four hundred ', 'five hundred ',
+                    'six hundred ',
+                    'seven hundred ', 'eight hundred ', 'nine hundred ']
+        comma = ['thousand ', 'million ', 'trillion ', 'quadrillion ']
+        word = ''
+        int_length = len(int_side)
+        dec_length = len(dec_side)
+        change = int_length
+        up_change = 0
+        while change > 0:
+            if int_side == '':
+                break
+            if number == '0':
+                word = 'zero'
+                break
+            elif change > 1 and int_side[change - 2] == '1':
+                for i in range(0, 10):
+                    if int_side[change - 1] == str(i):
+                        word = teens[i] + word
+            else:
+                if change > 0:
+                    for i in range(0, 10):
+                        if int_side[change - 1] == str(i):
+                            word = ones[i] + word
+                if change > 1:
+                    for i in range(0, 10):
+                        if int_side[change - 2] == str(i):
+                            word = decades[i] + word
+            if change > 2:
+                for i in range(0, 10):
+                    if int_side[change - 3] == str(i):
+                        word = hundreds[i] + word
+            if change > 3:
+                word = comma[up_change] + word
+            change -= 3
+            up_change += 1
+        # word += 'birr '
+
+        print(dec_side)
+        """
+        for i in range(0, len(dec_side)):
+            for x in range(0, 10):
+                if dec_side[i] == str(x):
+                    word += ones[x]"""
+
+        if dec_side not in ['', '0', '00']:
+            word += 'birr and '
+            word += self.convert_to_word(dec_side) + " cents"
+
+        word += " only"
+
+        return word.title()
 
 
 class account_move_line(models.Model):
