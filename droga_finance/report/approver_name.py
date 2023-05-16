d2d46ed4b719550b3cfdf359200471ea0b370b8e
Binyam Assefa <binyam@afomsoft.com>
2023-05-16 16:36:51 +0300

    payment request updated

--- droga_finance/report/approver_name.py
+++ droga_finance/report/approver_name.py
@@ -8,14 +8,14 @@ class ApproverName(models.Model):
 
     res_id = fields.Integer("res_id")
     create_uid = fields.Many2one("res.users")
-    models = fields.Char("Model")
+    model = fields.Char("Model")
     state = fields.Char("State")
 
     def get_approver(self, res_id, model, state):
         name = ''
 
         self.env.cr.execute(
-            """select * from droga_approver_name_view where  res_id=%s and models=%s and state=%s """, (res_id, model, state))
+            """select * from droga_approver_name_view where  res_id=%s and model=%s and state=%s """, (res_id, model, state))
         results = self.env.cr.dictfetchall()
 
         if results:
@@ -29,7 +29,7 @@ class ApproverName(models.Model):
         self._cr.execute(""" 
               create or replace view droga_approver_name_view as 
               (
-                   select distinct res_id,max(a.create_uid) as create_uid,models,new_value_char as state from mail_message a inner join mail_tracking_value b on a.id=b.mail_message_id
+                   select distinct res_id,max(a.create_uid) as create_uid,a.model,new_value_char as state from mail_message a inner join mail_tracking_value b on a.id=b.mail_message_id
                     group by res_id,model,new_value_char
               )
            """)
