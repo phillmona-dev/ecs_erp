a214793eb81f726b01a211f1842d5e8d4dc2c598
Moss <musse@afomsoft.com>
2023-06-16 03:24:29 -0700

    Ledger update to GIT

--- droga_inventory/models/update_foreign_supp.py
+++ droga_inventory/models/update_foreign_supp.py
@@ -8,3 +8,8 @@ class update_foreign(models.Model):
         supp=self.env['purchase.order'].search([('request_type','=','Foregin')]).mapped('partner_id')
         for partner in supp:
             partner.write({'property_stock_supplier': 263})
+    def update_ledger(self):
+        supp = self.env['purchase.order'].search([('request_type', '=', 'Foregin')]).mapped('partner_id')
+        acc_moves=self.env['account.move.line'].search([('partner_id','in',supp.ids),('account_id','=',2468),('company_id','=',1)])
+        for mv in acc_moves:
+            mv.write({'account_id':990})
