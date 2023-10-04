584fa0afe4c4c545394c83f8394420c717a827d6
Binyam Assefa <binyam@afomsoft.com>
2023-10-04 17:22:19 +0300

    Rounding problem for withholding solved

--- droga_finance/models/account_move.py
+++ droga_finance/models/account_move.py
@@ -99,9 +99,9 @@ class AccountMove(models.Model):
                 elif tax_id.amount == 15:
                     vat_amount += abs(record.balance * tax_id.amount / 100)
 
-        self.withholding_two_percent = tax_amount1
-        self.withholding_thirty_percent = tax_amount2
-        self.vat_percent = vat_amount
+        self.withholding_two_percent = round(tax_amount1, 2)
+        self.withholding_thirty_percent = round(tax_amount2, 2)
+        self.vat_percent = round(vat_amount, 2)
 
     # get sales person
     @api.depends('invoice_line_ids.analytic_distribution')
