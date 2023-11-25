359d647287d322526754e932e194dc407b03037d
Binyam Assefa <binyam@afomsoft.com>
2023-11-25 16:07:00 +0300

    attendance table updated

--- droga_hr/models/hr_attendance.py
+++ droga_hr/models/hr_attendance.py
@@ -1,6 +1,8 @@
-from odoo import models,fields,api
+from odoo import models, fields, api
+
 
 class Attendance(models.Model):
     _inherit = 'hr.attendance'
 
-    attendance_machine_trans_id=fields.Char("Attendance Machine Trans ID")
+    attendance_machine_trans_id = fields.Char("Attendance Machine Trans ID")
+    attendance_date = fields.Date("Attendance Date")
