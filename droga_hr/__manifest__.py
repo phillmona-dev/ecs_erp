cdd124100f88a83ea491406189489972fdc3a524
Binyam Assefa <binyam@afomsoft.com>
2023-12-02 11:30:47 +0300

    Attendance tree view and payment request locked ups

--- droga_hr/__manifest__.py
+++ droga_hr/__manifest__.py
@@ -31,6 +31,7 @@
         'views/hr_head_count_request.xml',
         'views/employee.xml',
         'views/hr_letter.xml',
+        'views/hr_attendance.xml',
         'report/letter.xml',
         'views/menu.xml',
     ],
