22a2bfd838b033a19e99356c93c14ec48e7ec93c
Binyam Assefa <binyam@afomsoft.com>
2023-12-18 16:30:01 +0300

    Department for HR Updated

--- droga_hr/models/employee.py
+++ droga_hr/models/employee.py
@@ -13,7 +13,7 @@ class Employee(models.Model):
 
     hire_date = fields.Date("Hire Date")
     retire_date = fields.Date("Retire Date", compute="compute_retire_date")
-    department_name = fields.Char(related='department_id.name')
+    department_name = fields.Char(related='department_id.name', store=True)
 
     bank = fields.Many2one('res.bank', string="Bank")
     bank_account = fields.Char("Bank Account")
