be6bdef115db7fec39cffb2af08fc1dea5d2fe3d
ERPslayer <112681942+ERPslayer@users.noreply.github.com>
2024-01-06 12:05:39 +0300

    Pharmacy changes

--- droga_pharma/models/reward_settings/droga_pharma_reward_gain_settings.py
+++ droga_pharma/models/reward_settings/droga_pharma_reward_gain_settings.py
@@ -9,3 +9,5 @@ class droga_pharma_reward_gain(models.Model):
     remark = fields.Char('Remark')
     status = fields.Selection([('Active', 'Active'), ('Closed', 'Closed')], required=True, default='Active',
                               tracking=True)
+    type=fields.Selection([('Stocked', 'Stocked'), ('Services', 'Services')], required=True, default='Stocked',
+                              tracking=True)
