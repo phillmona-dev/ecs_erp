bde29a513750bf807699c1a35f26bd9112d73679
ERPslayer <112681942+ERPslayer@users.noreply.github.com>
2024-10-01 08:32:57 +0300

    Pharmacy contract type

--- droga_pharma/models/reward_settings/droga_pharma_breast_feed_reward_contact_type.py
+++ droga_pharma/models/reward_settings/droga_pharma_breast_feed_reward_contact_type.py
@@ -7,6 +7,7 @@ class droga_pharma_feed_reward(models.Model):
     cont_type=fields.Selection([('bp', 'Breast feeding mom'), ('hp', 'Health professional')], required=True, string='Type',
                               tracking=True)
     discount=fields.Float('Discount rate',tracking=True)
+    pharmacy_group_id = fields.Many2many('droga.prod.categ.pharma','droga_breast_feed_rel','droga_breast_feed_id','pharmacy_group_id',store=True)
     remark = fields.Char('Remark')
     status = fields.Selection([('Active', 'Active'), ('Closed', 'Closed')], required=True, default='Active',
                               tracking=True)
