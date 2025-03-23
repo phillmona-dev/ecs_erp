791d56e3c7d3221883448d05a7371bd5dc1d0a7b
Binyam Assefa <binyam@afomsoft.com>
2025-03-23 22:11:57 +0300

    New vat reporting format for MOR added

--- droga_finance/report/witholding_report.py
+++ droga_finance/report/witholding_report.py
@@ -14,7 +14,8 @@ class WitholdingReport(models.TransientModel):
     _name = 'droga.finance.witholding.excel.report'
 
     report = fields.Selection(
-        [('Payable', 'Payable'), ('Receivable', 'Receivable')])
+        [('Withholding Payable', 'Withholding Payable'), ('Withholding Receivable', 'Withholding Receivable'),
+         ('VAT Receivable', 'VAT Receivable'),('VAT Payable', 'VAT Payable')])
     date_from = fields.Date("Date From", required=True)
     date_to = fields.Date("Date To")
     company_id = fields.Many2one('res.company', 'Company', required=True,
@@ -27,10 +28,14 @@ class WitholdingReport(models.TransientModel):
         # This generates our Excel file
         file_io = BytesIO()
         workbook = xlsxwriter.Workbook(file_io)
-        if self.report == 'Payable':
+        if self.report == 'Withholding Payable':
             self.generate_payable_xlsx_report(workbook)
-        else:
+        elif self.report == 'Withholding Receivable':
             self.generate_receivable_xlsx_report(workbook)
+        elif self.report == 'VAT Receivable':
+            self.generate_vat_xlsx_report(workbook, 'in_invoice', 'Purchase VAT 15%')
+        elif self.report == 'VAT Payable':
+            self.generate_vat_xlsx_report(workbook, 'out_invoice', 'Sales VAT 15%')
         workbook.close()
 
         # The file to download will be stored under fileout field
@@ -39,7 +44,7 @@ class WitholdingReport(models.TransientModel):
 
         # The file name is stored under filename
         datetime_string = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-        filename = '%s_%s' % ('Witholding Report ', datetime_string)
+        filename = '%s_%s_%s' % (self.report, ' Report ', datetime_string)
 
         # This downloads file. The file is fileout and the name if filename
         return {
@@ -121,8 +126,8 @@ class WitholdingReport(models.TransientModel):
         accounts = self.env['account.account'].search(
             [('code', 'in', ('214003', '214004'))])
 
-        withholdings = account_moves.line_ids.search(
-            [('account_id', 'in', accounts.ids)])
+        withholdings = self.env['account.move.line'].search(
+            [('move_id', 'in', account_moves.ids), ('account_id', 'in', accounts.ids)])
 
         sheet.write(row_start, 0, 'Withholder''s Tax Account', title_format)
         sheet.write(row_start, 1, 'Withholdee''s TIN', title_format)
@@ -223,8 +228,8 @@ class WitholdingReport(models.TransientModel):
         accounts = self.env['account.account'].search(
             [('code', 'in', ('116001', '116002'))])
 
-        withholdings = account_moves.line_ids.search(
-            [('account_id', 'in', accounts.ids)])
+        withholdings = self.env['account.move.line'].search(
+            [('move_id', 'in', account_moves.ids), ('account_id', 'in', accounts.ids)])
 
         sheet.write(row_start, 0, 'Withholder''s Tax Account', title_format)
         sheet.write(row_start, 1, 'Withholdee''s TIN', title_format)
@@ -251,4 +256,127 @@ class WitholdingReport(models.TransientModel):
             sheet.write(row_start, 5, record.tax_base_amount, num_format)
             sheet.write(row_start, 6, abs(record.balance), num_format)
             sheet.write(row_start, 7, internal_ref, border)
+            row_start += 1
+
+    def generate_vat_xlsx_report(self, workbook, move_type, tax_type):
+        sheet = workbook.add_worksheet('VAT Receivable')
+
+        sheet.set_column('A:A', 25)
+        sheet.set_column('B:B', 15)
+        sheet.set_column('C:C', 30)
+        sheet.set_column('D:D', 15)
+        sheet.set_column('E:E', 15)
+        sheet.set_column('F:F', 20)
+        sheet.set_column('G:G', 20)
+        sheet.set_column('H:H', 20)
+        row_start = 0
+        date_format = workbook.add_format(
+            {'num_format': 'dd/mm/yyyy', 'border': 7})
+        num_format = workbook.add_format({'num_format': 43, 'border': 7})
+        cent_format = workbook.add_format({'num_format': 41, 'border': 7})
+        border = workbook.add_format({'border': 7})
+        bold = workbook.add_format({'bold': True})
+        header_format = workbook.add_format({
+            'bold': 1,
+            'border': 0,
+            'align': 'center',
+            'valign': 'vcenter',
+            'font_size': 22})
+        main_title_format = workbook.add_format({
+            'bold': 1,
+            'border': 0,
+            'align': 'center',
+            'valign': 'vcenter',
+            'font_size': 16})
+        parameter_format = workbook.add_format({
+            'bold': 1,
+            'border': 7,
+            'align': 'left',
+            'valign': 'vcenter',
+            'font_size': 12,
+            'fg_color': '#F6F5F5'})
+
+        separator_format = workbook.add_format({
+            'bold': 1,
+            'border': 7,
+            'align': 'left',
+            'valign': 'vcenter',
+            'font_size': 12,
+            'fg_color': '#D9D9D9'})
+
+        title_format = workbook.add_format({
+            'bold': 1,
+            'border': 1,
+            'align': 'center',
+            'valign': 'vcenter',
+            'font_size': 11,
+            'text_wrap': 1,
+            'fg_color': '#F6F5F5'})
+        title_format_num = workbook.add_format({
+            'bold': 1,
+            'border': 1,
+            'num_format': 43,
+            'align': 'center',
+            'valign': 'vcenter',
+            'font_size': 11,
+            'text_wrap': 1,
+            'fg_color': '#F6F5F5'})
+
+        # search RFQ
+
+        account_moves = self.env['account.move'].search(
+            [('date', '>=', self.date_from), ('date', '<=', self.date_to), ('move_type', '=', move_type),
+             ('company_id', '=', self.company_id.id)])
+
+        tax_ids = self.env['account.tax'].search(
+            [('name', '=', tax_type), ('company_id', '=', self.company_id.id)])
+
+        lines = self.env['account.move.line'].search(
+            [('move_id', 'in', account_moves.ids), ('display_type', '=', 'product'), ('tax_ids', 'in', tax_ids.ids)])
+
+        sheet.write(row_start, 0, 'VATS Category', title_format)
+        sheet.write(row_start, 1, 'Calendar Type', title_format)
+        sheet.write(row_start, 2, 'Types of purchase (1-5)', title_format)
+        sheet.write(row_start, 3, 'TIN', title_format)
+        sheet.write(row_start, 4, 'Seller name (for imported purchase only)', title_format)
+        sheet.write(row_start, 5, 'Date of purchase/Customs Declaration No. Dispatched Date', title_format)
+        sheet.write(row_start, 6, 'MRC Number', title_format)
+        sheet.write(row_start, 7, 'Vat receipt number/ Customs Declaration Number(Fs)', title_format)
+        sheet.write(row_start, 8, 'Description', title_format)
+        sheet.write(row_start, 9, 'Unit of Measure (type ID 2-10)', title_format)
+        sheet.write(row_start, 10, 'Quantity', title_format)
+        sheet.write(row_start, 11, 'Unit Price', title_format)
+        sheet.write(row_start, 12, 'Total value before VAT', title_format)
+        sheet.write(row_start, 13, 'VAT paid on purchase Inputs or VAT Paid on imported goods', title_format)
+        sheet.write(row_start, 14, 'Gross Input Value', title_format)
+        row_start += 1
+
+        for record in lines:
+            # get reciept info from fs model
+
+            # get supplier name and tin no
+            supplier_name = record.move_id.partner_id.name if record.move_id.partner_id.name else ''
+            supplier_tin = record.move_id.partner_id.vat if record.move_id.partner_id.vat else ''
+            reciept_no = record.move_id.withholding_no if record.move_id.withholding_no else ''
+            internal_ref = record.move_id.withholding_internal_ref if record.move_id.withholding_internal_ref else ''
+            FSInvoiceNumber = record.move_id.FSInvoiceNumber if record.move_id.FSInvoiceNumber else ''
+            FTimeStamp = record.move_id.FTimeStamp if record.move_id.FTimeStamp else ''
+            FPMachineID = record.move_id.FPMachineID if record.move_id.FPMachineID else ''
+
+            sheet.write(row_start, 0, 'G', border)
+            sheet.write(row_start, 1, 'G', border)
+            sheet.write(row_start, 2, '1', border)
+            sheet.write(row_start, 3, supplier_tin, border)
+            sheet.write(row_start, 4, supplier_name, border)
+            sheet.write(row_start, 5, FTimeStamp, date_format)
+            sheet.write(row_start, 6, FPMachineID, border)
+            sheet.write(row_start, 7, FSInvoiceNumber, border)
+
+            sheet.write(row_start, 8, record.name, border)
+            sheet.write(row_start, 9, record.product_uom_id.name, border)
+            sheet.write(row_start, 10, record.quantity, num_format)
+            sheet.write(row_start, 11, record.price_unit, num_format)
+            sheet.write(row_start, 12, record.price_subtotal, num_format)
+            sheet.write(row_start, 13, record.price_subtotal * 1.15 - record.price_subtotal, num_format)
+            sheet.write(row_start, 14, record.price_subtotal * 1.15, num_format)
             row_start += 1
