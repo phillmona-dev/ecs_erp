167f5b6fa644ee9d144acd4a12afd22d00896882
ERPslayer <112681942+ERPslayer@users.noreply.github.com>
2025-03-26 03:03:54 +0300

    Start build

--- droga_inventory/models/update_accounts.py
+++ droga_inventory/models/update_accounts.py
@@ -43,7 +43,7 @@ class update_acc(models.Model):
         date_limit = datetime.date(2024, 7, 7)
         moves = self.env['droga.stock.valuation.layer'].search([('move_date', '>', date_limit), ('company_id','=',1),('account_move_id', '=', False)], limit=1000)
         #moves = self.env['droga.stock.valuation.layer'].search(
-        #     [('id','=',296275)], limit=100)
+        #    [('id','=',296275)], limit=100)
         for ret in moves:
             ret._validate_accounting_entries_custom()
             for svl in ret:
