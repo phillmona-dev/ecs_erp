6e1e98afd6ce81c187ae891c1cfce487a0c9269c
Binyam Assefa <binyam@afomsoft.com>
2026-01-08 10:50:16 +0300

    Payment report follow-up updated in the better way

--- droga_finance/report/payment_report.py
+++ droga_finance/report/payment_report.py
@@ -57,89 +57,82 @@ class PaymentReport(models.Model):
                         break
 
     def init(self):
-        drop_view_if_exists(self.env.cr, 'droga_finance_payment_report')
+        cr = self.env.cr
+
+        # Drop any existing view or materialized view safely
+        cr.execute("""
+               DROP MATERIALIZED VIEW IF EXISTS droga_finance_payment_report CASCADE;
+               DROP VIEW IF EXISTS droga_finance_payment_report CASCADE;
+           """)
+
+        # Create the materialized view with row_number()
+        cr.execute("""
+               CREATE MATERIALIZED VIEW droga_finance_payment_report AS
+               SELECT
+                   row_number() OVER () AS id,
+                   ap.id AS payment_id,
+                   am.id AS move_id,
+                   am.company_id,
+                   ap.partner_id,
+                   CASE
+                       WHEN ap.payment_type = 'inbound' THEN 'Customer'
+                       ELSE 'Vendor'
+                   END AS payment_type,
+                   ap.category,
+                   ap.division,
+                   ap.sales_channel,
+                   am.name AS invoice_no,
+                   am.sales_type,
+                   am.sales_initiator,
+                   am.invoice_date_due,
+                   apr.max_date AS paid_date,
+                   CASE
+                       WHEN ap.payment_type = 'inbound' THEN am.amount_total_signed
+                       ELSE abs(am.amount_total_signed)
+                   END AS total_amount,
+                   ap.amount AS paid_amount,
+                   apr.amount AS settled_amount,
+                   (apr.max_date - am.invoice_date_due) AS due_days,
+                   (CURRENT_DATE - apr.max_date) AS paid_passed_days
+               FROM account_move am
+               JOIN account_move_line aml
+                   ON aml.move_id = am.id
+                  AND aml.display_type = 'payment_term'
+               JOIN account_partial_reconcile apr
+                   ON apr.debit_move_id = aml.id
+                   OR apr.credit_move_id = aml.id
+               JOIN account_move_line pml
+                   ON (pml.id = apr.credit_move_id OR pml.id = apr.debit_move_id)
+                  AND pml.payment_id IS NOT NULL
+               JOIN account_payment ap
+                   ON ap.id = pml.payment_id
+               WHERE am.state = 'posted';
+           """)
+
+        # Create supporting indexes for performance
+        cr.execute("""
+        
+                CREATE UNIQUE INDEX idx_dfpr_id
+                ON droga_finance_payment_report (id);
+        
+               CREATE INDEX idx_dfpr_partner
+               ON droga_finance_payment_report (partner_id);
+
+               CREATE INDEX idx_dfpr_company
+               ON droga_finance_payment_report (company_id);
+
+               CREATE INDEX idx_dfpr_paid_date
+               ON droga_finance_payment_report (paid_date);
+           """)
+
+    @api.model
+    def refresh_payment_report(self):
+        """
+        Refresh materialized view without blocking users
+        """
         self.env.cr.execute("""
-            CREATE OR REPLACE VIEW droga_finance_payment_report AS (
-
-                /* ================= CUSTOMER PAYMENTS ================= */
-                SELECT
-                    row_number() OVER () AS id,
-                    ap.partner_id,
-                    'Customer' AS payment_type,
-                    ap.category,
-                    ap.division,
-                    ap.sales_channel,
-                    am.name AS invoice_no,
-                    am.sales_type,
-                    am.sales_initiator,
-                    am.invoice_date_due,
-                    apr.max_date AS paid_date,
-                    am.amount_total_signed AS total_amount,
-                    ap.amount AS paid_amount,
-                    apr.amount AS settled_amount,
-                    (apr.max_date - am.invoice_date_due) AS due_days,
-                    (CURRENT_DATE - apr.max_date) AS paid_passed_days,
-                    am.company_id
-
-                FROM account_move am
-                JOIN account_move_line aml
-                    ON aml.move_id = am.id
-                   AND aml.display_type = 'payment_term'
-
-                JOIN account_partial_reconcile apr
-                    ON apr.debit_move_id = aml.id
-
-                JOIN account_move_line pml
-                    ON pml.id = apr.credit_move_id
-                   AND pml.payment_id IS NOT NULL
-
-                JOIN account_payment ap
-                    ON ap.id = pml.payment_id
-                   AND ap.payment_type = 'inbound'
-
-                WHERE am.state = 'posted'
-
-                UNION ALL
-
-                /* ================= VENDOR PAYMENTS ================= */
-                SELECT
-                    row_number() OVER () AS id,
-                    ap.partner_id,
-                    'Vendor' AS payment_type,
-                    ap.category,
-                    ap.division,
-                    ap.sales_channel,
-                    am.name AS invoice_no,
-                    am.sales_type,
-                    am.sales_initiator,
-                    am.invoice_date_due,
-                    apr.max_date AS paid_date,
-                    abs(am.amount_total_signed) AS total_amount,
-                    ap.amount AS paid_amount,
-                    apr.amount AS settled_amount,
-                    (apr.max_date - am.invoice_date_due) AS due_days,
-                    (CURRENT_DATE - apr.max_date) AS paid_passed_days,
-                    am.company_id
-
-                FROM account_move am
-                JOIN account_move_line aml
-                    ON aml.move_id = am.id
-                   AND aml.display_type = 'payment_term'
-
-                JOIN account_partial_reconcile apr
-                    ON apr.credit_move_id = aml.id
-
-                JOIN account_move_line pml
-                    ON pml.id = apr.debit_move_id
-                   AND pml.payment_id IS NOT NULL
-
-                JOIN account_payment ap
-                    ON ap.id = pml.payment_id
-                   AND ap.payment_type = 'outbound'
-
-                WHERE am.state = 'posted'
-            )
-        """)
+                REFRESH MATERIALIZED VIEW CONCURRENTLY droga_finance_payment_report;
+            """)
 
 
 class AccountPaymentLinK(models.Model):
